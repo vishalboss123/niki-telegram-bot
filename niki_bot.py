75d3d4d230d705fe0e2da41ab8dbf58124b8ca94
vishalboss123 <subhabishalbehera@gmail.com>
2026-05-12 19:13:56 +0530

    Update niki_bot.py

--- niki_bot.py
+++ niki_bot.py
@@ -8044,7 +8044,7 @@ Current Message:
 
         response = client_ai.chat.completions.create(
 
-            model="deepseek/deepseek-chat-v3-0324:free",
+            model="mistralai/mistral-7b-instruct:free",
 
             messages=[
                 {
