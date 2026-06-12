65ae0cb2d6ed9763974e388ba0710a9946bdbcd1
vishalboss123 <subhabishalbehera@gmail.com>
2026-06-13 05:01:12 +0530

    Update leaderboard.py

--- leaderboard.py
+++ leaderboard.py
@@ -1,1 +1,16 @@
+from PIL import Image, ImageDraw
 
+def test_leaderboard():
+    img = Image.new("RGB", (1200, 800), (20, 20, 20))
+
+    draw = ImageDraw.Draw(img)
+
+    draw.text(
+        (50, 50),
+        "TOP RICHEST PLAYERS",
+        fill=(255, 215, 0)
+    )
+
+    img.save("leaderboards/test.png")
+
+    return "leaderboards/test.png"
